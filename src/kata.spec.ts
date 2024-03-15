1a568f0ba3ca23bdd2a2f3d7b80354da5549409f
Brandon Schenz <bschenz@gmail.com>
2024-03-15 12:22:51 -0400

    wonderland-number

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -7,14 +7,7 @@ beforeEach(() => {
 });
 
 describe('Kata Tests', () => {
-    it.each`
-        name         | expected
-        ${'Ingage'}  | ${'Hello Ingage'}
-        ${'Brandon'} | ${'Hello Brandon'}
-    `(
-        'returns $expected when $name is passed to function',
-        ({ name, expected }) => {
-            expect(fixture.hello(name)).toEqual(expected);
-        }
-    );
+    it('should find the wonderland number', () => {
+        expect(fixture.findWonderlandNumber()).toEqual(142857);
+    });
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,44 @@
+// import * as _ from 'lodash';
+
 export class Kata {
-    public hello(name: string): string {
-        return `Hello ${name}`;
+    // Function to get digits of a number
+    private digits = (n: number): number[] => n.toString().split('').map(Number)
+
+    // Function to check if two lists are permutations of each other
+    private permutation(l1: number[], l2: number[]): boolean {
+        if (l1.length !== l2.length) return false;
+
+        const sorted1 = l1.slice().sort();
+        const sorted2 = l2.slice().sort();
+
+        for (let i = 0; i < sorted1.length; i++) {
+            if (sorted1[i] !== sorted2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Function to check if a number is a wonderland number
+    public wonderland(n: number): boolean {
+        const nums = [2 * n, 3 * n, 4 * n, 5 * n, 6 * n];
+        const nDigits = this.digits(n);
+        return nums.every(num => this.permutation(this.digits(num), nDigits));
+    }
+
+    // Function to find the first wonderland number
+    public findWonderlandNumber(): [number, number[]] {
+        let n = 100_000;
+        let matches = 0;
+        let matchNumbers = [];
+        while (n < 1_000_000_000) {
+            if (this.wonderland(n)) {
+                matches++
+                matchNumbers.push(n)
+            }
+            n++;
+        }
+        //return -1; // No wonderland number found
+        return [matches, matchNumbers];
     }
 }
